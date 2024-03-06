5c18f6db88b798297c65233e31a7b66bad4169c6
Gregory <Gregory@ZENBOOK>
2024-03-06 15:23:46 +0300

    Добавьте файлы проекта.

---
+++ лаб 2 задание 3/лаб 2 задание 3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cctype>
+using namespace std;
+void replaceDigits(const char* inputFilename, const char* outputFilename) {
+    ifstream inputFile(inputFilename);
+    if (!inputFile.is_open()) {
+        cout << "Ошибка открытия файла " << inputFilename << endl;
+        return;
+    }
+
+    
+    cout << "файл " << inputFilename << ":" << std::endl;
+    char ch;
+    while (inputFile.get(ch)) {
+        cout << ch;
+    }
+    inputFile.close();
+
+    ofstream outputFile(outputFilename);
+
+    inputFile.open(inputFilename); 
+    while (inputFile.get(ch)) {
+        if (isdigit(ch)) {
+            outputFile << '*';
+        }
+        else {
+            outputFile << ch;
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    ifstream outputFileCheck(outputFilename);
+    
+    cout << endl;
+    cout << "файл " << outputFilename << ":" << endl;
+    while (outputFileCheck.get(ch)) {
+        cout << ch;
+    }
+    outputFileCheck.close();
+
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const char* inputFilename = "a3.txt";
+    const char* outputFilename = "a4.txt";
+
+    replaceDigits(inputFilename, outputFilename);
+
+    return 0;
+}
